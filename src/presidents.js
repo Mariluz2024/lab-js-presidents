a657ab7a5958ff39e994ce7aa48fea341bc5e7e1
Mariluz <mluzco29@gmail.com>
2024-11-25 08:23:48 +0100

    done

--- src/presidents.js
+++ src/presidents.js
@@ -419,25 +419,46 @@ const presidents = [
 
 
 // Iteration 1 | Names of All Presidents - `map()`
-function getNames(presidentsArr) {}
+//function getNames(presidentsArr) {}
+function getNames(presidents) {
+  return presidents.map(president => president.name);
+  }
 
-
+  console.log( getNames(presidents) );
 
 
 // Iteration 2 | Democratic Presidents - `filter()`
-function getDemocraticPresidents(presidentsArr) {}
+//function getDemocraticPresidents(presidentsArr) {}
+
+function getDemocraticPresidents(presidents) {
+  return presidents.filter(president => president.party === "Democratic");
+}
 
 
 
 
 // Iteration 3 | Count Years in Office - reduce()
-function  countYearsInOffice(presidentsArr) {}
+//function  countYearsInOffice(presidentsArr) {}
+
+function countYearsInOffice(presidents) {
+  return presidents.reduce((totalYears, president) => {
+    
+    if (president.leftOffice !== null) {
+      return totalYears + (president.leftOffice - president.tookOffice);
+    }
+    return totalYears;
+  }, 0); 
+}
 
 
 
 
 // Iteration 4 | Sort Presidents by Birth Year - `sort()`
-function sortPresidentsByBirthYear(presidentsArr) {}
+//function sortPresidentsByBirthYear(presidentsArr) {}
+
+function sortPresidentsByBirthYear(presidents) {
+  return presidents.sort((a, b) => a.birthYear - b.birthYear);
+}
 
 
 
